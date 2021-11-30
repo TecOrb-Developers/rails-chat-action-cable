659bd19e3b3c560f233e936b7af7dd2d41048fb9
Priya <mini@tecorb.co>
2021-11-30 11:29:09 +0530

    doorkeeper updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+if Doorkeeper::Application.count.zero?
+	application = Doorkeeper::Application.create({
+						name: "iOS client",
+						scopes: "read write",
+						confidential: false,
+						redirect_uri: "urn:ietf:wg:oauth:2.0:oob"
+					})
+	
+	# Doorkeeper::Application.create(name: "iOS client", redirect_uri: "", scopes: "")
+	# Doorkeeper::Application.create(name: "Android client", redirect_uri: "", scopes: "")
+	# Doorkeeper::Application.create(name: "React", redirect_uri: "", scopes: "")
+
+	# get apps credientials
+	# client_id of the application
+	Doorkeeper::Application.find_by(name: "Android client").uid
+
+	# client_secret of the application
+	Doorkeeper::Application.find_by(name: "Android client").secret
+
+end
+
+
+# User.create({
+# 	email: "jai@tecorb.co",
+# 	password: "123456",
+# 	fname: "Jai",
+# 	lname: "Rajput",
+# 	country_code: "+91",
+# 	mobile_number: "7834821711",
+# 	govt_id: "122123"
+# })
