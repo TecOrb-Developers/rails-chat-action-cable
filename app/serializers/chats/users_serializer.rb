227063ea8529873d07efcee847a5749496f101b0
Jai <jai@tecorb.co>
2022-09-26 14:59:45 +0530

    Decorators added for User model

--- app/serializers/chats/users_serializer.rb
+++ app/serializers/chats/users_serializer.rb
@@ -1,3 +1,15 @@
 class Chats::UsersSerializer < ApplicationSerializer
-	attributes :id, :fname, :lname, :mobile_number, :country_code
+	attributes :id, :full_name, :contact_number, :joined_at
+
+	def full_name
+		object.decorate.full_name
+	end
+
+	def joined_at
+		object.decorate.joined_at
+	end
+
+	def contact_number
+		object.decorate.contact_number
+	end
 end
