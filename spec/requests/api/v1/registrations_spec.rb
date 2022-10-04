60ce265591d1022b4841e6b80c03e08f1e955f68
Jai <jai@tecorb.co>
2022-10-04 16:26:33 +0530

    Build/Create factory in specs example

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -1,7 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Registrations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/v1/registrations" do
+    context 'Testing validations' do
+      it 'Success' do
+        user = create(:user)
+      end
+
+      it 'Blacklisted email' do
+        user = create(:user)
+      end
+
+      it 'Blacklisted domain' do
+        user = create(:user)
+      end
+
+      it 'First name with number' do
+        user = create(:user)
+      end
+
+      it 'Last name with number' do
+        user = create(:user)
+      end
+      
+      it 'Missing country code' do
+        user = create(:user)
+      end
+
+      it 'Missing mobile number' do
+        user = create(:user)
+      end 
+
+      it 'Missing email' do
+        user = create(:user)
+      end 
+
+      it 'Duplicate mobile number with same country code' do
+        user = create(:user)
+      end
+
+      it 'Duplicate mobile number with different country code' do
+        user = create(:user)
+      end  
+
+      it 'Duplicate email' do
+        user = create(:user)
+      end
+    end
   end
 end
